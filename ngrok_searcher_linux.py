8a7999954da9b9a4e5d9fad9b8a9c02c956ca6e4
DPr1son <77262664+DPr1son@users.noreply.github.com>
2021-01-11 09:44:55 +0300

    Add files via upload

---
+++ ngrok_searcher_linux.py
@@ -0,0 +1,22 @@
+import requests
+import random
+import colorama
+import sys
+file = open('workings_sites_ngrok.txt', 'a')
+g = str("qwertyuiopasdfghjklzxcvbnm1234567890")
+
+def gen():
+	global g
+	i = str("http://" + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g) + random.choice(g) + ".ngrok.io")
+	check(i)
+def check(url):
+	r = requests.get(url)
+	print("\033[33m" + url)
+	if r.status_code == 404:
+		print("\033[31mСайт не рабочий!")
+	else:
+		print("\033[32mСайт рабочий!")
+		file.write('\n' + url)
+	gen()
+sys.setrecursionlimit(999999999)
+gen()
